d3d1720185b03eb08a51899a74388314fe5c2f95
Harry <harryzhu45@gmail.com>
2025-12-06 15:06:17 -0800

    add pw confirm

--- web_sample/app/register.tsx
+++ web_sample/app/register.tsx
@@ -64,6 +64,7 @@ export default function RegisterScreen() {
         username: username.trim(),
         email: email.trim(),
         password,
+        password_confirm: confirmPassword,
       });
       console.log('Registration successful');
       
